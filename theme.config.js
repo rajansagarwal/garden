35e6f7bf62d89f710cd94cc347b13022aa08b07c
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-11-12 23:43:40 +0000

    ples work

--- theme.config.js
+++ theme.config.js
@@ -15,14 +15,12 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Writings & Posts - Rajan Agarwal" />
+      <meta name="og:description" content="A collection of thoughts, writings and ideas" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:site:domain" content="posts.heyrajan.com" />
+      <meta name="twitter:url" content="https://posts.heyrajan.com" />
+      <meta name="og:title" content="Writings // Rajan Agarwal" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
