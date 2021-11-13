9bbff04f5645d114a3cfaa4f2ed4384506dd67a5
Rajan Agarwal <64426829+itsrajan@users.noreply.github.com>
2021-11-13 18:06:20 +0000

    index page && meta

--- theme.config.js
+++ theme.config.js
@@ -1,60 +1,29 @@
 export default {
   github: 'https://github.com/itsrajan/blog',
-  titleSuffix: ' – Rajan Agarwal',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Digital Garden</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Rajan Agarwal</span>
       <span className="text-gray-600 font-normal hidden md:inline"><a href="https://heyrajan.com" style={{textDecoration: 'none', color: 'gray'}}>
-        Rajan Agarwal 
+        My Digital Garden 
       </a></span>
     </>
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="msapplication-TileColor" content="#000" />
+      <meta name="theme-color" content="#000" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Writings & Posts - Rajan Agarwal" />
-      <meta name="og:description" content="A collection of thoughts, writings and ideas" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site:domain" content="posts.heyrajan.com" />
-      <meta name="twitter:url" content="https://posts.heyrajan.com" />
-      <meta name="og:title" content="Writings // Rajan Agarwal" />
+      <meta name="description" content="Rajan Agarwal is a 16 year old hacktivist based in Toronto." />
+      <meta name="og:description" content="A collection of thoughts, writings, projects and ideas" />
+      <meta name="twitter:site:domain" content="heyrajan.com" />
+      <meta name="twitter:url" content="https://heyrajan.com" />
+      <meta name="og:title" content="Rajan Agarwal" />
       <meta name="apple-mobile-web-app-title" content="Rajan Agarwal" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+			<title>Rajan Agarwal - Author, Developer, Hacktivist</title>
     </>
   ),
-  search: false,
+  search: true,
   prevLinks: true,
   nextLinks: true,
 	font: false,
