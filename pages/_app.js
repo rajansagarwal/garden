e70f5cbd162d80b774c58419932d5aaa4b208f71
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-11-21 00:38:09 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import 'nextra-theme-docs/style.css';
 import '../App.css';
+import ForceTheme from '../components/force-theme'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <ForceTheme theme="dark" />
+      <Component {...pageProps} />
+    </>
+  )
 }
