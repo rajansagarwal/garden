6af0bb2af1e79b14d2a2144cf843b2fe152a75b4
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-11-21 00:30:39 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import 'nextra-theme-docs/style.css';
 import '../App.css';
+import { ThemeProvider } from 'next-themes'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
